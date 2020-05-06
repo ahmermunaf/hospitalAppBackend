eee575719f460d88a2fe5ad237f1facad642e748
Catalyse Developer <dev@catalyse.business>
2020-05-07 03:16:37 +0500

    completed

---
+++ client/build/precache-manifest.2ae96d26c81b8f4ab510a3b1c6178f8e.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "442315083ddfb441abe9c4806dcc9cbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44532bbc9b93fa4c9381",
+    "url": "/static/css/2.8e352bc1.chunk.css"
+  },
+  {
+    "revision": "930b3d4bfaf633cf97e2",
+    "url": "/static/css/main.8c4c28eb.chunk.css"
+  },
+  {
+    "revision": "44532bbc9b93fa4c9381",
+    "url": "/static/js/2.2fdb56be.chunk.js"
+  },
+  {
+    "revision": "df8be702f0cc9d299c0ff42ca03d3e40",
+    "url": "/static/js/2.2fdb56be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "930b3d4bfaf633cf97e2",
+    "url": "/static/js/main.e3b7aab8.chunk.js"
+  },
+  {
+    "revision": "8ad0a82b866fe80b5f42",
+    "url": "/static/js/runtime-main.8a1090fe.js"
+  },
+  {
+    "revision": "27e3649bab7c62fa21b8837c4842e40e",
+    "url": "/static/media/Montserrat-Black.27e3649b.ttf"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "19ba7aa52a78c3896558ac1c0a5fb4c7",
+    "url": "/static/media/Montserrat-ExtraBold.19ba7aa5.ttf"
+  },
+  {
+    "revision": "a7063e0c0f0cb546ad45e9e24b27bd3b",
+    "url": "/static/media/Montserrat-Italic.a7063e0c.ttf"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/static/media/Montserrat-Light.409c7f79.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/static/media/Montserrat-SemiBold.c641dbee.ttf"
+  },
+  {
+    "revision": "43dd5b7a3d277362d5e801e5353e3a01",
+    "url": "/static/media/Montserrat-Thin.43dd5b7a.ttf"
+  },
+  {
+    "revision": "aef35107abb0a2cfa555e8a656972446",
+    "url": "/static/media/banner3.aef35107.jpg"
+  },
+  {
+    "revision": "4c5d5fdc65e6794d05da423ccd1c3a49",
+    "url": "/static/media/error-404.4c5d5fdc.svg"
+  },
+  {
+    "revision": "7fc7bf87dceff65d23fbe60c593b4fc7",
+    "url": "/static/media/facebook.7fc7bf87.svg"
+  },
+  {
+    "revision": "90de31e4e5befcee2d80c5df0a17a68d",
+    "url": "/static/media/instagram.90de31e4.svg"
+  },
+  {
+    "revision": "87b7d54e41db73a04262fa2b85a38988",
+    "url": "/static/media/logo.87b7d54e.png"
+  },
+  {
+    "revision": "cc64731605e60950fdc9387ffdb73417",
+    "url": "/static/media/twitter.cc647316.svg"
+  },
+  {
+    "revision": "fab0b3b627dcf69866f82be53d90bf1a",
+    "url": "/static/media/youtube.fab0b3b6.svg"
+  }
+]);
